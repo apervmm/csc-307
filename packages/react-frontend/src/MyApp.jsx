4679857618a7a5ffafc8691e5166d17e36c62ab5
Almas <apervmm@gmail.com>
2024-09-30 23:49:00 -0700

    Form

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Table from "./Table";
+import Form from "./Form";
 
 // const characters = [
 //   {
@@ -22,24 +23,7 @@ import Table from "./Table";
 
 
 function MyApp() {
-  const [characters, setCharacters] = useState([
-    {
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -55,6 +39,7 @@ function MyApp() {
         characterData = {characters} 
         removeCharacter = {removeOneCharacter}
       />
+      < Form />
     </div>
   );
 }

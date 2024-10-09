621f1b128e00dda57e6b0a337fa7a1d044552f79
Almas <apervmm@gmail.com>
2024-10-09 14:58:24 -0700

    -{id}

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -40,8 +40,22 @@ app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+};
+
 app.get("/users", (req, res) => {
-    res.send(users);
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
 });
 
 app.listen(port, () => {

c6a1b24787d0d274918cf2055148a51940c909b4
Almas <apervmm@gmail.com>
2024-10-09 14:18:55 -0700

    users

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -5,8 +5,43 @@ const port = 8000;
 
 app.use(express.json());
 
+
+const users = {
+    users_list: [
+      {
+        id: "xyz789",
+        name: "Charlie",
+        job: "Janitor"
+      },
+      {
+        id: "abc123",
+        name: "Mac",
+        job: "Bouncer"
+      },
+      {
+        id: "ppp222",
+        name: "Mac",
+        job: "Professor"
+      },
+      {
+        id: "yat999",
+        name: "Dee",
+        job: "Aspring actress"
+      },
+      {
+        id: "zap555",
+        name: "Dennis",
+        job: "Bartender"
+      }
+    ]
+  };
+
 app.get("/", (req, res) => {
     res.send("Hello, World!");
+});
+
+app.get("/users", (req, res) => {
+    res.send(users);
 });
 
 app.listen(port, () => {

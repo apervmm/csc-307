5050be97b912e6a208a9212c05b1d46a291da01d
Almas <apervmm@gmail.com>
2024-10-09 21:48:22 -0700

    id endpoint

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -58,6 +58,19 @@ app.get("/users", (req, res) => {
     }
 });
 
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 app.listen(port, () => {
     console.log(
         `Example app listening at http://localhost:${port}`

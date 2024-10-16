ec9d74a51b47044cba962c50451c77466a957ace
Almas <apervmm@gmail.com>
2024-10-16 14:51:21 -0700

    updateList

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -32,15 +32,19 @@ function MyApp() {
     setCharacters(updated);
   }
 
-  function updateList(person){
-    setCharacters([...characters, person]);
+  function updateList(person) {
+    postUser(person)
+      .then(() => setCharacters([...characters, person]))
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
-  
+
   useEffect(() => {
     fetchUsers()
       .then((res) => res.json())
@@ -49,6 +53,19 @@ function MyApp() {
         console.log(error);
       });
   }, []);
+
+
+  function postUser(person) {
+    const promise = fetch("Http://localhost:8000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(person)
+    });
+  
+    return promise;
+  }
 
   return (
     <div className={"conainer"}>
